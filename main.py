0867d6a470adaa2b3243590a0553973483fc4239
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-08-22 17:11:05 +0200

    voeg comments en een clear command toe aan main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 filename :str = input("bestandnaam van de code: ")
-max_index :int = 100
 pointer :int = 0
-data_list :list[int] = [0] * max_index
+data_list :list[int] = [0] * 100
+loop :list = []
 
-
-def main(pointer:int, filename:str, max_index:int, data_list :list) -> None:
+def main(pointer:int, filename:str, data_list :list, loop :list) -> None:
   # error handeling
   try:
       with open(filename) as file:
@@ -18,21 +17,38 @@ def main(pointer:int, filename:str, max_index:int, data_list :list) -> None:
           if not letter:
               print(data_list)
               break
-          if letter == ">" and not pointer == max_index - 1:
+          if letter == ">":
+              # voeg 1 toe aan de pointer met 1
               pointer += 1
-          elif letter == "<" and not pointer == 0:
+              if pointer == len(data_list):
+                  data_list.append(0)
+          elif letter == "<":
+              # verminder de pointer met 1
               pointer -= 1
+              if pointer < 0:
+                  data_list.insert(0,0)
           elif letter == "+":
+              # voeg 1 toe aan de huidige cel
               data_list[pointer] += 1
-          elif letter == "-" and not data_list[pointer] == 0:
+              if data_list[pointer] == 256:
+                  data_list[pointer] = 0
+          elif letter == "-":
+              # verminder de huidige cel met 1
               data_list[pointer] -= 1
+              if data_list[pointer] == 0:
+                  data_list[pointer] = 255
           elif letter == ",":
+              # input int command
               int_input :int = int(input("input int: "))
               data_list[pointer] = int_input
           elif letter == ".":
+              # output command
               print(chr(data_list[pointer]))
+          elif letter == "/":
+              # clear command
+              data_list = [0] * len(data_list)
           else:
               continue
       return
 
-main(pointer,filename,max_index,data_list)
+main(pointer,filename,data_list,loop)
