a33305e6091fb5b320c27e6ccbb30706d4bda539
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-11-16 12:16:00 +0100

    voeg extra commands toe

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-import threading
+import os
 
 filename :str = input("bestandnaam van de code: ")
 pointer :int = 0
 data_list :list[int] = [0] * 100
-loop :list = []
 
 def error_handeling(filename:str) -> None:
   try:
       with open(filename) as file:
           file.close()
+          print("bestand gevonden")
   except FileNotFoundError:
       print("bestand niet gevonden")
 
-def main(pointer:int, filename:str, data_list :list, loop :list) -> None:
-  error_handeling(filename)
+def main(pointer:int, filename:str, data_list :list) -> None:
   with open(filename) as file:
       letter :int = 0
       code :list[str] = file.read().splitlines()
       while letter != len(code) + 1:
+          
+          # pointer functies
           if code[letter] == ">":
-              # voeg 1 toe aan de pointer met 1
+              # voeg 1 toe aan de pointer
               pointer += 1
               if pointer == len(data_list):
                   data_list.append(0)
@@ -28,6 +29,8 @@ def main(pointer:int, filename:str, data_list :list, loop :list) -> None:
               pointer -= 1
               if pointer < 0:
                   data_list.insert(0,0)
+
+          # reken functies
           elif code[letter] == "+":
               # voeg 1 toe aan de huidige cel
               data_list[pointer] += 1
@@ -38,6 +41,16 @@ def main(pointer:int, filename:str, data_list :list, loop :list) -> None:
               data_list[pointer] -= 1
               if data_list[pointer] == 0:
                   data_list[pointer] = 255
+          elif code[letter] == "*":
+              # vermenigvuldig het getal op en voor de pointer met elkaar
+              data_list[pointer] = data_list[pointer -1] * data_list[pointer]
+          elif code[letter] == "/":
+              # deel het getal op en voor de pointer met elkaar
+              # het deeltal is het getal voor de pointer
+              # de deler is het getal op de pointer
+              data_list[pointer] = data_list[pointer -1] / data_list[pointer]
+
+          # andere commands
           elif code[letter] == ",":
               # input int command
               int_input :int = int(input("input int: "))
@@ -45,7 +58,7 @@ def main(pointer:int, filename:str, data_list :list, loop :list) -> None:
           elif code[letter] == ".":
               # output command
               print(chr(data_list[pointer]))
-          elif code[letter] == "/":
+          elif code[letter] == "µ":
               # clear data command
               data_list :int = [0] * len(data_list)
           elif code[letter] == ";":
@@ -61,22 +74,12 @@ def main(pointer:int, filename:str, data_list :list, loop :list) -> None:
               pointer -= data_list[pointer]
               while pointer < 0:
                   pointer += 1
-          elif code[letter] == "*":
-              # vermenigvuldig het getal op en voor de pointer met elkaar
-              data_list[pointer] = data_list[pointer -1] * data_list[pointer]
-          elif code[letter] == "/":
-              # deel het getal op en voor de pointer met elkaar
-              # het deeltal is het getal voor de pointer
-              # de deler is het getal op de pointer
-              data_list[pointer] = data_list[pointer -1] / data_list[pointer]
+
           else:
               continue
           letter + 1
       print(data_list)
       return
 
-error_handel_thread :threading.Thread = threading.Thread(target=error_handeling, args=[filename])
-main_thread :threading.Thread = threading.Thread(target=main,args=[pointer,filename,data_list,loop])
-
-error_handel_thread.start()
-main_thread.start()
+error_handeling(filename)
+main(pointer,filename,data_list)
